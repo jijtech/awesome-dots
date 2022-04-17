0ee513b9d129ee72cec1e5496300205355fffff3
Milo staack <66209480+jijtech@users.noreply.github.com>
2022-04-17 15:09:39 +0000

    Add files via upload

--- polybar/scripts/memory.sh
+++ polybar/scripts/memory.sh
@@ -4,7 +4,7 @@ case "$1" in
         notify-send "Memory (%)" "$(ps axch -o cmd:10,pmem k -pmem | head | awk '$0=$0"%"' )"
         ;;
     *)
-        echo " $(free -h --si | awk '/^Mem:/ {print $3 "/" $2}')"
+        echo "RAM $(free -h --si | awk '/^Mem:/ {print $3 "/" $2}')"
         ;;
 esac
 
