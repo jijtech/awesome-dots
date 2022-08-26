704074099ed08af5446d0d843fcd2ead76efa8ef
Milo staack <66209480+jijtech@users.noreply.github.com>
2022-08-26 12:28:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,9 @@ cp -r zathura $HOME/.config/
 cp -r alacritty $HOME/.config/
 cp -r picom.conf $HOME/.config/
 cd ../
-rm -rf awesome-dots
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Agave.zip
+unzip Agave.zip -d $HOME/.fonts/
+fc-cache -vf
+rm -rf dots
 
 echo "Install done! Do 'xrdb ~/.Xresources' then Logout or Restart WM"
