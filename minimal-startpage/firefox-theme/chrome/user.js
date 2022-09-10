ba576ff4b773641949994494bb18e575eea906b2
Milo staack <66209480+jijtech@users.noreply.github.com>
2022-09-10 18:58:47 +0200

    Add files via upload

---
+++ minimal-startpage/firefox-theme/chrome/user.js
@@ -0,0 +1,5 @@
+// Applying setting to enable minimal theme
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("layers.acceleration.force-enabled",  false);
+user_pref("gfx.webrender.all",  true);
+user_pref("svg.context-properties.content.enabled", true)
