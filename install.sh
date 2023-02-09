445b29452a167956f19099a3b2d9a9e8de3cb88d
Milo staack <66209480+jijtech@users.noreply.github.com>
2023-02-09 16:37:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 ###########>INSTALLATION<#############
 ######################################
 
+#moving stuff
 cp -r polybar $HOME/.config/
 cp -r rofi $HOME/.config/
 cp -r awesome $HOME/.config/
@@ -13,9 +14,18 @@ cp -r alacritty $HOME/.config/
 cp -r picom.conf $HOME/.config/
 cp -r gtk-3.0 $HOME/.config/
 cd ../
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Agave.zip
-unzip Agave.zip -d $HOME/.fonts/
+
+#Getting fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Ubuntu.zip
+unzip Ubuntu.zip -d $HOME/.fonts/
 fc-cache -vf
+
+#Grapping the terminal
+wget https://github.com/Eugeny/tabby/releases/download/v1.0.188/tabby-1.0.188-linux-x64.rpm
+sudo dnf install tabby-1.0.188-linux-x64.rpm
+
+#finishing cleanup
 rm -rf dots
 
+#report completion
 echo "Install done! Do 'xrdb ~/.Xresources' then Logout or Restart WM"
