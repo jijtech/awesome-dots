41e8052c866825f0b5ca1cfb7c4fb54174f26c80
Milo staack <66209480+jijtech@users.noreply.github.com>
2023-07-31 19:19:05 +0200

    installing my apps sute

---
+++ Flatpak-apps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "staring install of apps"
+
+flatpak install flathub com.bitwarden.desktop -y 
+flatpak install flathub org.keepassxc.KeePassXC -y                      
+flatpak install flathub com.discordapp.Discord -y                     
+flatpak install flathub com.github.tchx84.Flatseal -y                   
+flatpak install flathub com.moonlight_stream.Moonlight -y                 
+flatpak install flathub com.visualstudio.code -y              
+flatpak install flathub net.lutris.Lutris -y                       
+flatpak install flathub org.bleachbit.BleachBit -y                       
+flatpak install flathub org.kde.isoimagewriter -y                       
+flatpak install flathub org.mozilla.Thunderbird -y                       
+flatpak install flathub org.mozilla.firefox -y                           
+flatpak install flathub org.onlyoffice.desktopeditors -y                 
+flatpak install flathub org.qbittorrent.qBittorrent -y                   
+flatpak install flathub org.signal.Signal -y
+flatpak install flathub sh.cider.Cider -y                                 
